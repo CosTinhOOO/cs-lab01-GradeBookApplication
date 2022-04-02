568d61f5689ae82f2b42073b04099737964f92b0
CosTinhOOO <kostek.karagiannakos@gmail.com>
2022-04-02 19:24:02 +0200

    Override `RankedGradeBook`'s `GetLetterGrade` method
    Overriding `RankedGradeBook`'s `GetLetterGrade` method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,47 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            double top20 = Students.Count * 0.2;
+            double top40 = Students.Count * 0.4;
+            double top60 = Students.Count * 0.6;
+            double top80 = Students.Count * 0.8;
+
+            List<double> grades = new List<double>();
+            List<double> gradesSorted = new List<double>();
+            
+            int i = 0;
+            foreach(Student student in Students)
+            {
+                if (student.AverageGrade > averageGrade) { i++; }
+            }
+
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            else if (i < top20)
+            {
+                return 'A';
+            }
+            else if(i <  top40 && i > top20)
+            {
+                return 'B';
+            }
+            else if (i > top40 && i < top60)
+            {
+                return 'C';
+            }
+            else if (i > top60 && i < top80)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+
+        }
     }
 }
